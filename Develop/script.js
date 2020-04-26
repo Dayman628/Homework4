e20fdcc15f2c054a11e40e5548d2005dc5b1f30d
DAYMAN <andrew.morrow5510@gmail.com>
2020-04-26 14:45:14 -0700

    added new functions for pulling questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,135 +1,34 @@
 // This is the homework to create a quiz through dynamically updated html using js   
-
-
-(function(){
-    
-    var quizContainer = document.getElementById('quiz');
-    var resultsContainer = document.getElementById('results');
-    var submitButton = document.getElementById('submit');
-    
-    function buildQuiz(){
-          // variable to store the HTML output
-          var output = [];
-      
-          // for each question...
-          myQuestions.forEach((currentQuestion, questionNumber) => {
-      
-              // variable to store the list of possible answers
-              var answers = [];
-      
-              // and for each available answer...
-              for(letter in currentQuestion.answers){
-      
-                // ...add an HTML radio button
-                answers.push(
-                  `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                  </label>`
-                );
-              }
-      
-              // add this question and its answers to the output
-              output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
-              );
-            }
-          );
-      
-          // finally combine our output list into one string of HTML and put it on the page
-          quizContainer.innerHTML = output.join('');
-        }
-      
-        function showResults(){
-      
-          // gather answer containers from our quiz
-          var answerContainers = quizContainer.querySelectorAll('.answers');
-      
-          // keep track of user's answers
-          var numCorrect = 0;
-      
-          // for each question...
-          myQuestions.forEach( (currentQuestion, questionNumber) => {
-      
-            // find selected answer
-            var answerContainer = answerContainers[questionNumber];
-            var selector = `input[name=question${questionNumber}]:checked`;
-            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-      
-            // if answer is correct
-            if(userAnswer === currentQuestion.correctAnswer){
-              
-              // add to the number of correct answers
-              numCorrect++;
-      
-              // color the answers green
-              answerContainers[questionNumber].style.color = 'lightgreen';
-            }
-              // if answer is wrong or blank
-              else{
-              // color the answers red
-              answerContainers[questionNumber].style.color = 'red';
-            }
-          });
-      
-          // show number of correct answers out of total
-          resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-        }
-        var myQuestions = [
-          {
-            question: "Who invented JavaScript?",
-            answers: {
-              a: "Douglas Crockford",
-              b: "Sheryl Sandberg",
-              c: "Brendan Eich"
-            },
-            correctAnswer: "c"
-          },
-          {
-            question: "Which one of these is a JavaScript package manager?",
-            answers: {
-              a: "Node.js",
-              b: "TypeScript",
-              c: "npm"
-            },
-            correctAnswer: "c"
-          },
-          {
-            question: "Which tool can you use to ensure code quality?",
-            answers: {
-              a: "Angular",
-              b: "jQuery",
-              c: "RequireJS",
-              d: "ESLint"
-            },
-            correctAnswer: "d"
-          }
-        ];
-      
-        // Kick things off
-        buildQuiz();
-      
-        // Event listeners
-        submitButton.addEventListener('click', showResults);
-
-        document.addEventListener('click', function (event) {
-
-            if (!event.target.matches('.start-quiz')) return;
-            
-            var fiveMinutes = 2 * 5,
-            display = document.querySelector('#timer');
-            startTimer(fiveMinutes, display);
-    
+    (function() {
+        var questions = [{
+          question: "What is 2*5?",
+          choices: [2, 5, 10, 15, 20],
+          correctAnswer: 2
+        }, {
+          question: "What is 3*6?",
+          choices: [3, 6, 9, 12, 18],
+          correctAnswer: 4
+        }, {
+          question: "What is 8*9?",
+          choices: [72, 99, 108, 134, 156],
+          correctAnswer: 0
+        }, {
+          question: "What is 1*7?",
+          choices: [4, 5, 6, 7, 8],
+          correctAnswer: 3
+        }, {
+          question: "What is 8*8?",
+          choices: [20, 30, 40, 50, 64],
+          correctAnswer: 4
+        }];
         
-            // Log the clicked element in the console
-            console.log(event.target);
-        
-        }, false);
+        var questionCounter = 0; //Tracks question number
+        var selections = []; //Array containing user choices
+        var quiz = $('#quiz'); //Quiz div object
         
         function startTimer(duration, display) {
             var timer = duration, minutes, seconds;
+            $('#starter').hide();
             var callback = callback || function(){
                 console.log('timer done')
             };
@@ -147,5 +46,171 @@
                 duration-- || (clearInterval(timerCountdown), callback());
             }, 1000);
         }
+
+        document.addEventListener('click', function (event) {
+             
+
+            if (!event.target.matches('.start-quiz')) return;
+            
+            var fiveMinutes = 2 * 5,
+            display = document.querySelector('#timer');
+             // Display initial question
+            displayNext();
+            startTimer(fiveMinutes, display);
     
+        
+            // Log the clicked element in the console
+            console.log(event.target);
+        
+        }, false);
+
+       
+        
+        // Click handler for the 'next' button
+        $('#next').on('click', function (e) {
+          e.preventDefault();
+          
+          // Suspend click listener during fade animation
+          if(quiz.is(':animated')) {        
+            return false;
+          }
+          choose();
+          
+          // If no user selection, progress is stopped
+          if (isNaN(selections[questionCounter])) {
+            alert('Please make a selection!');
+          } else {
+            questionCounter++;
+            displayNext();
+          }
+        });
+        
+        // Click handler for the 'prev' button
+        $('#prev').on('click', function (e) {
+          e.preventDefault();
+          
+          if(quiz.is(':animated')) {
+            return false;
+          }
+          choose();
+          questionCounter--;
+          displayNext();
+        });
+        
+        // Click handler for the 'Start Over' button
+        $('#start').on('click', function (e) {
+          e.preventDefault();
+          
+          if(quiz.is(':animated')) {
+            return false;
+          }
+          questionCounter = 0;
+          selections = [];
+          displayNext();
+          $('#start').hide();
+        });
+        
+        // Animates buttons on hover
+        $('.button').on('mouseenter', function () {
+          $(this).addClass('active');
+        });
+        $('.button').on('mouseleave', function () {
+          $(this).removeClass('active');
+        });
+        
+        // Creates and returns the div that contains the questions and 
+        // the answer selections
+        function createQuestionElement(index) {
+          var qElement = $('<div>', {
+            id: 'question'
+          });
+          
+          var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+          qElement.append(header);
+          
+          var question = $('<p>').append(questions[index].question);
+          qElement.append(question);
+          
+          var radioButtons = createRadios(index);
+          qElement.append(radioButtons);
+          
+          return qElement;
+        }
+        
+        // Creates a list of the answer choices as radio inputs
+        function createRadios(index) {
+          var radioList = $('<ul>');
+          var item;
+          var input = '';
+          for (var i = 0; i < questions[index].choices.length; i++) {
+            item = $('<li>');
+            input = '<input type="radio" name="answer" value=' + i + ' />';
+            input += questions[index].choices[i];
+            item.append(input);
+            radioList.append(item);
+          }
+          return radioList;
+        }
+        
+        // Reads the user selection and pushes the value to an array
+        function choose() {
+          selections[questionCounter] = +$('input[name="answer"]:checked').val();
+        }
+        
+        // Displays next requested element
+        function displayNext() {
+          quiz.fadeOut(function() {
+            $('#question').remove();
+            
+            if(questionCounter < questions.length){
+              var nextQuestion = createQuestionElement(questionCounter);
+              quiz.append(nextQuestion).fadeIn();
+              if (!(isNaN(selections[questionCounter]))) {
+                $('input[value='+selections[questionCounter]+']').prop('checked', true);
+              }
+              
+              // Controls display of 'prev' button
+              if(questionCounter === 1){
+                $('#prev').show();
+              } else if(questionCounter === 0){
+                
+                $('#prev').hide();
+                $('#next').show();
+              }
+            }else {
+              var scoreElem = displayScore();
+              quiz.append(scoreElem).fadeIn();
+              $('#next').hide();
+              $('#prev').hide();
+              $('#start').show();
+            }
+          });
+        }
+        
+        // Computes score and returns a paragraph element to be displayed
+        function displayScore() {
+            
+          var score = $('<p>',{id: 'question'});
+          var numCorrect = 0;
+          
+          
+
+          for (var i = 0; i < selections.length; i++) {
+            if (selections[i] === questions[i].correctAnswer) {
+              numCorrect++;
+            }
+          }
+          if (localStorage.getItem('highScore') == null) {
+            localStorage.setItem('highScore', numCorrect);
+          }
+          
+          else if (numCorrect > localStorage.getItem('highScore')) {
+            localStorage.setItem('highScore', numCorrect);
+          }
+
+          score.append('You got ' + numCorrect + ' questions out of ' +
+                       questions.length + ' right!!!')
+            
+          return score;
+        }
     })();
